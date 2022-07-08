605eeb1a6ef1ad5e96aa84ab6f7bc71a77c19c25
Ridercogswell <ridercogswell@gmail.com>
2022-07-07 17:04:23 -0700

    interfaces for appstate and props

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,18 @@ interface AppProps {
   color?: string;
 }
 
-class App extends React.Component<AppProps> {
-  state = { counter: 0 };
+interface AppState {
+  counter: number;
+}
+
+
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+
+    this.state = { counter: 0 };
+  }
 
   onIncrement = (): void => {
     this.setState({ counter: this.state.counter + 1 })
