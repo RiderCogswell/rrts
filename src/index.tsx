9df3f592e846e36900ffd2f63244ce7b6f89a1a1
Ridercogswell <ridercogswell@gmail.com>
2022-07-07 20:37:16 -0700

    reducers

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,9 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { App } from "./components/App";
+import { reducers } from "./reducers";
 
-const store = createStore(reducers, applyMiddleware)
+const store = createStore(reducers, applyMiddleware(thunk))
 
 ReactDOM.render(
   <App />,
