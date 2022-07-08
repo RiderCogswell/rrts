d66a6e8e0576291a9fb42db444fa75ab8fd36fd1
Ridercogswell <ridercogswell@gmail.com>
2022-07-07 20:32:48 -0700

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,46 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { App } from "./components/App";
 
-interface AppProps {
-  // made color optional
-  color?: string;
-}
-
-// functional component takes in props and outputs a jsx element
-// const App = (props: AppProps): JSX.Element => {
-//   return <div>{props.color}</div>
-// }
-
-// class App extends React.Component<AppProps> {
-//   // we could also set the state without AppState interface with this syntax below
-//   // could also be overrode by changing prop names
-//   state = { counter: 0 };
-//   // we'll use the easy route and avoid constructors
-//   // constructor(props: AppProps) {
-//   //   super(props);
-
-//   //   this.state = { counter: 0 };
-//   // }
-
-//   onIncrement = (): void => {
-//     this.setState({ counter: this.state.counter + 1 })
-//   }
-//   onDecrement = (): void => {
-//     this.setState({ counter: this.state.counter - 1 })
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <button onClick={this.onIncrement}>Increment</button>
-//         <button onClick={this.onDecrement}>Decrement</button>
-//         {this.state.counter}
-//       </div>
-//     )
-//   }
-// }
+const store = createStore(reducers, applyMiddleware)
 
 ReactDOM.render(
-  <App color="red"/>,
+  <App />,
   document.querySelector('#root')
 );
