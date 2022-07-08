cb38610a3a56e7b312b8f4b1541d462f26d6bdc5
Ridercogswell <ridercogswell@gmail.com>
2022-07-08 01:45:56 -0700

    add connect

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { connect } from "react-redux";
+import { Todo, fetchTodos } from "../actions";
+import { StoreState } from "../reducers";
 
-export class App extends React.Component {
+interface AppProps {
+  todo: Todo[];
+  fetchTodos(): any;
+}
+
+export class _App extends React.Component<AppProps> {
   render() {
-    return <div>Hi</div>
+    return (
+      <></>
+    )
   }
 }
+
+// destructure todos out of state and set it equal to itself
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return { todos };
+};
+
+// first parentheses is for configuration, second is for the connected component
+export const App = connect( mapStateToProps, { fetchTodos })(_App)
